c7d1631feae00410871dd196d2a972cce2e7f32c
Carly McKelvy <carlymckelvy@hotmail.com>
2020-07-10 11:40:16 -0700

    comment structure for PyPoll

---
+++ PyPoll/main.py
@@ -0,0 +1,3 @@
+#Copy csv file to PyPoll folder
+import shutil
+newpath = shutil.copy("C:/Users/carly/Documents/bootcamp/myfork/02-Homework/03-Python/Instructions/PyPoll/Resources/election_data.csv","C:/Users/carly/Documents/bootcamp/myfork/python-challenge/PyPoll/Resources")
