a5259d5d050c1028a6fb599d38e34dcd1943cb1b
Carly McKelvy <carlymckelvy@hotmail.com>
2020-07-10 17:52:33 -0700

    candidate list working

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,6 @@
 #Copy csv file to PyPoll folder
-import shutil
-newpath = shutil.copy("C:/Users/carly/Documents/bootcamp/myfork/02-Homework/03-Python/Instructions/PyPoll/Resources/election_data.csv","C:/Users/carly/Documents/bootcamp/myfork/python-challenge/PyPoll/Resources")
+# import shutil
+# newpath = shutil.copy("C:/Users/carly/Documents/bootcamp/myfork/02-Homework/03-Python/Instructions/PyPoll/Resources/election_data.csv","C:/Users/carly/Documents/bootcamp/myfork/python-challenge/PyPoll/Resources")
 
 #Import modules
 import os
@@ -10,10 +10,21 @@ import csv
 election_data = os.path.join("Resources", "election_data.csv")
 
 #Assign values to variables
+Voter_ID = []
+County = []
+candidate = []
+voter_count = 0
+current_row = 0
+prev_row = 0
+candidate_list = []
+Khan_votes = 0
+Correy_votes = 0
+Li_votes = 0
+
 
 
 #Read the csv file
-with open(election_data) as csvfile:
+with open(election_data, 'r') as csvfile:
 
     #Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -22,21 +33,58 @@ with open(election_data) as csvfile:
     header = next(csvreader)
 
     #Loop through the data
+    for row in csvreader:
+
+        #Find total number of votes        
+        voter_count = voter_count + 1
+
+        #candidate_list
+        candidate.append(row[2])
+        
+
+    for new in candidate:
+        if new not in candidate_list:
+            candidate_list.append(new)
+    for new in candidate_list:
+        print(new)
 
 
-        #Find total number of votes
+        # current_row = str(row[2])
+        # prev_row = (current_row - 1)
+
+        # if row[2] == prev_row[2]:
+        #     continue
+
+        # else: 
+        #     candidate_list.append(row[2])
+
+ 
+
+     
+        #Create a list of candidates who recevied votes from index 2
+        
+
+    #     #Determine total number of votes each candidate won
+    #     #For loop if index 2 = candidate name, then add to counter
+    #     #elseif = diff name, add 
+    #     if row[2] 
+
+        
+
+            
+         
+    
+
+    #Determine unique list of candidates
 
 
-        #Create list of candidates who recevied votes
+    
+
+    #Determine percentage of votes each candidate won
+        
 
 
-        #Determine percentage of votes each candidate won
-
-
-        #Determine total number of votes each candidate won
-
-
-        #Determine winner of election based on popluar vote
+    #Determine winner of election based on popluar vote
 
 
     #Print election results
